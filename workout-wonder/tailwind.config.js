8968c4249bf952538b4ba637713ebe55f11c260e
AngerOverApathy <violasian@gmail.com>
2023-11-02 16:32:03 -0700

    initial media queries added

--- workout-wonder/tailwind.config.js
+++ workout-wonder/tailwind.config.js
@@ -1,9 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCC",
+        "gray-500": "#5E00070",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD58",
+        "secondary-500": "#FFC132"
+      },
+      backgroundColor:(theme) => ({
+        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')"
+      }),
+      fontFamily: {
+        "dmsans": ["DM Sans", "sans-serif"],
+        "monsterrat": ["Monsterrat", "serif"]
+      },
+      content: {
+        evolvetext: "url('./assetsEvolveText.png')",
+        abstractwaves: "url('./AbstractWaves.png')",
+        sparkles: "url('./Sparkles.png')",
+        circles: "url('./Circles.png')"
+      }
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px"
+    }
   },
   plugins: [],
 }
-
