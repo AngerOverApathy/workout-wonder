805ae273a54d81758f7cbb0eed1f7efb73624f3b
AngerOverApathy <violasian@gmail.com>
2023-11-14 20:33:19 -0800

    heading created

--- workout-wonder/src/scenes/home/index.tsx
+++ workout-wonder/src/scenes/home/index.tsx
@@ -21,13 +21,15 @@ const Home = ({ setSelectedPage }: Props) => {
         className='gap-16 bg-gray-20 pb-10 py-10 md:h-full md:pb-0'
     >
         {/* IMAGE AND MAIN HEADER */}
-        <div>
+        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
             {/* MAIN HEADER */}
-            <div>
+            <div className='z-10 mt-32 md:basis-3/5'>
                 {/* HEADER */}
-                <div>
-                    <div>
-                        <img alt="home-page-text" src={HomePageText} />
+                <div className='md:-mt-20'>
+                    <div className='relative'>
+                        <div className='before:absolute before:-top-20 before:content-evolvetext'>
+                            <img alt="home-page-text" src={HomePageText} />
+                        </div>
                     </div>
                     <p>
                     Welcome to Atlas Athletic Training Gym, your ultimate destination for fitness and performance excellence. At Atlas, we're not just a gym; we're a community committed to helping you unlock your potential and achieve your fitness dreams.
@@ -66,9 +68,7 @@ const Home = ({ setSelectedPage }: Props) => {
                     </div>
                 </div>
             </div>
-
-        )
-    }
+        )}
     </section>
 }
 
