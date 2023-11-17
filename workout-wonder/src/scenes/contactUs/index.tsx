bcbb50d6f2cbe6c50a81d3f1f429c090b0185b91
AngerOverApathy <violasian@gmail.com>
2023-11-16 16:09:04 -0800

    contact us form created

--- workout-wonder/src/scenes/contactUs/index.tsx
+++ workout-wonder/src/scenes/contactUs/index.tsx
@@ -1,6 +1,6 @@
 import { SelectedPage } from '@/shared/types'
 import { motion } from 'framer-motion'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import ContactUsPageGraphic from '@/assets/ContactUsPageGraphic.png'
 import HText from '@/shared/HText'
 
