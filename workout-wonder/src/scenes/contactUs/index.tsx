b9259b2a7eb037022bf2ac7a2f5d99e59677309e
AngerOverApathy <violasian@gmail.com>
2023-11-16 16:40:49 -0800

    contact form created and styled

--- workout-wonder/src/scenes/contactUs/index.tsx
+++ workout-wonder/src/scenes/contactUs/index.tsx
@@ -11,7 +11,7 @@ type Props = {
 const ContactUs = ({ setSelectedPage }: Props) => {
 
     const inputStyles = `
-    w-full rounded-lg bg-primary-300 
+    mb-5 w-full rounded-lg bg-primary-300 
     px-5 py-3 placeholder-white
     `
 
@@ -104,10 +104,11 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                                 </p>
                             )}
 
-                            <input
+                            <textarea
                                 className={inputStyles}
-                                type='text'
                                 placeholder='MESSAGE'
+                                rows={4}
+                                cols={50}
                                 {...register('message', { 
                                     required: true,
                                     maxLength: 2000,
@@ -124,10 +125,30 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                             type='submit'
                             className='mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white'
                             >
-                                SUBMIT
+                            SUBMIT
                             </button>
 
                         </form>
+                    </motion.div>
+
+                    <motion.div
+                        className='relative mt-16 basis-2/5 md:mt-0'
+                        initial='hidden'
+                        whileInView='visible'
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, y: 50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}
+                    >
+                        <div className='md:before:content-evolvetext w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1]'>
+                            <img 
+                                className='w-full'
+                                alt='contact-us-page-graphic'
+                                src={ContactUsPageGraphic}
+                            />
+                        </div>
                     </motion.div>
                 </div>
             </motion.div>
